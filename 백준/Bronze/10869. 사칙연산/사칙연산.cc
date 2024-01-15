2877b75bc4be20e95d4335658e671897703249cd
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2024-01-15 20:12:41 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main() {
+	int a, b{};
+
+	std::cin >> a >> b;
+
+	std::cout << a+b <<'\n';
+	std::cout << a - b << '\n';
+	std::cout << a*b << '\n';
+	std::cout << a/b << '\n';
+	std::cout << a%b << '\n';
+	
+	
+	return 0 ;
+
+}
