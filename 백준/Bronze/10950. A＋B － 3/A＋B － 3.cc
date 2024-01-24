ae36036e6744d21b16eaa9e0d445c5138049255b
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2024-01-24 17:14:40 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    
+    int T;
+   
+    std::cin >> T;
+
+    std::vector<std::pair<int, int>> testCases;
+
+
+    for (int i = 0; i < T; ++i) {
+        int A, B;
+
+        std::cin >> A >> B;
+        testCases.push_back(std::make_pair(A, B));
+    }
+
+    // 각 테스트 케이스의 결과 출력
+    for (int i = 0; i < T; ++i) {
+ 
+        std::cout << testCases[i].first + testCases[i].second << std::endl;
+    }
+
+    return 0;
+}
