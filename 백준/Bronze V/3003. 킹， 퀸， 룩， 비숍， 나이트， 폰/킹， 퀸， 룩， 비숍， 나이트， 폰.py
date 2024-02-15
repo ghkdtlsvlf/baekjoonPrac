1b6665cc850c17e80646c8707c34d594ec4016d9
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2024-02-15 17:12:38 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze V/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze V/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -4,10 +4,6 @@ chessPieceNumber = list(map(int, input().split()))
 originNumber = (1, 1, 2, 2, 2, 8)
 
 for i in range(len(chessPieceNumber)):
-    if chessPieceNumber[i] == originNumber[i]:
-        print('0',end=" ")
-      
-    else:
-        print(originNumber[i] - chessPieceNumber[i],end=" ")
-   
+    print(originNumber[i] - chessPieceNumber[i],end=" ")
+
 
