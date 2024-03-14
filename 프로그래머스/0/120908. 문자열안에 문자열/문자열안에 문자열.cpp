1a27e5a8e82244efe64416e3ba9d33571d09d01b
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2024-03-14 13:40:26 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(string str1, string str2) {
+    int answer = 0;
+
+    auto result = str1.find(str2);
+    if (result != string::npos) answer = 1;
+    else answer = 2;
+
+
+    return answer;
+}
