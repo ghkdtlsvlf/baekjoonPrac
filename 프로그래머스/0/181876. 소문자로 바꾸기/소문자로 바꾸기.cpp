44949f958d4a80f8ea7c0a91d7da59905f19afbb
ghkdtlsvlf <56142400+ghkdtlsvlf@users.noreply.github.com>
2024-03-21 11:29:54 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+
+string solution(string myString) {
+    string answer = "";
+
+    
+    for (char ch : myString)
+    {
+     answer +=tolower(ch);
+    }
+    return answer;
+}
